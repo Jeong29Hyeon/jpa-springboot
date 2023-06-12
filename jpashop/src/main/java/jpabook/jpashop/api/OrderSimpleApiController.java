364717e7a9d9acb3063013b15afa4b13b39b3c0d
jeong29hyeon <sian0369@naver.com>
2023-06-12 22:51:12 +0900

    #19 - Order API V2 개발
    기존 엔티티를 반환하던 V1 -> DTO 변환해서 반환
    FetchType 이 LAZY 이기 때문에 DTO에서 연관관계에 있는 member, delivery 를 사용하는 시점에 select 쿼리가 추가로 날라가게 된다. N + 1 문제 발생
    + EntityManager 는 @Transactional 안에서 쓰이고 이를 통해 영속성 컨텍스트에 없을 때 DB에서 조회해 오는데 어떻게 가능하지? 이 점은 OSIV 와 성능 최적화 편에서 알아 볼 수있다고 한다!!

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne (ManyToOne, OneToOne)
@@ -21,12 +26,38 @@ public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
 
     @GetMapping("/api/v1/simple-orders")
-    public List<Order> ordersv1(){
+    public List<Order> ordersV1(){
         List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
         for (Order order : all) {
             order.getMember().getName(); //LAZY LOADING 이였던걸 강제 초기화
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        // N + 1 문제 -> 처음 order(2개) 조회하는 sql 쿼리 -> 회원,배송에 대한 쿼리 2개씩 총 4개의 쿼리가 더 나감.
+        List<Order> result = orderRepository.findAllByCriteria(new OrderSearch());
+        return result.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address= order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 }
